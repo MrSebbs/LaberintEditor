6cb35c525968af84192e97d923e93254ea9e4842
MrSebbs <martiserrallonga@gmail.com>
2019-09-29 12:06:14 +0200

    Standby project: SVG Transform

--- misc/SVG Transform/Transform Vectors/Transform.js
+++ misc/SVG Transform/Transform Vectors/Transform.js
@@ -1,103 +1,148 @@
+// text.split(/[a-zA-Z]/);
+
+class VectorSVG{
+	constructor(type, vec){
+		this.type = type;
+		this.vec = vec;
+	}
+}
+
+class ReaderSVG{
+	constructor(){
+		this.strPath = [];
+		this.path = [];
+	}
+
+	regexSearch(text, regex){
+		var result = [];
+		var find = regex.exec(text);
+		while (find != null){
+			result.push(find[0]);
+			find = regex.exec(text);
+		}
+		return result;
+	}
+
+	regexParse(strSVG){
+		const regexPathSVG = /<path (.*)d=(.*)>/gim;
+		const regexPathMZ = /m([^z]*)z/gim;
+		const regexSplit = /[a-zA-Z]([^a-zA-Z]*)(?=[a-zA-Z])/gm;
+		const regexFloat = /(-?(\d)+(\.?(\d)+)?)/gm;
+
+		var pathSVG = this.regexSearch(strSVG, regexPathSVG);
+		if(pathSVG.length == 0) pathSVG = strSVG;
+		pathSVG = pathSVG.toString();
+
+		var pathMZ = this.regexSearch(pathSVG, regexPathMZ);
+		for(var i=0; i<pathMZ.length; i++){
+			this.strPath.push(this.regexSearch(pathMZ[i], regexSplit));
+		}
+
+		var strElement, type, vec;
+
+		for(var i=0; i<this.strPath.length; i++){
+			pathMZ = this.strPath[i];
+			for(var j=0; j<pathMZ.length; j++){
+				strElement = pathMZ[j];
+				type = strElement[0];
+				// type = type.toUpperCase();
+				vec = this.regexSearch(strElement, regexFloat);
+				this.path.push(new VectorSVG(type, vec));
+			}
+			this.path.push(new VectorSVG('Z', []));
+		}
+	}
+
+	round(value){
+		var floatNum;
+		var strValue;
+
+		var integer, decimal, decimalValue, result;
+
+		var path = this.path;
+		for(var i=0; i<path.length; i++){
+			for(var v=0; v<path[i].vec.length; v++){
+				floatNum = path[i].vec[v];
+				strValue = floatNum.toString();
+
+				strValue = strValue.split('.');
+				if(strValue.length == 1){
+					console.log(strValue[0]);
+					console.log("Rounded");
+					continue;
+				}else if(strValue.length == 2){
+					integer = strValue[0];
+					decimal = strValue[1];
+
+					console.log(strValue);
+					console.log(integer);
+					console.log(decimal);
+
+					result = decimal.slice(0, value);
+					console.log(result);
+					if(decimal[value+1]>=5){
+						decimalValue = parseInt(decimal[value]);
+						decimalValue ++;
+						decimalValue 
+					}
+
+				}
+			}
+		}
+	}
+
+	print(){
+		var result = '';
+		var path = this.path;
+		for(var i=0; i<path.length; i++){
+			result += path[i].type +' ';
+			for(var v=0; v<path[i].vec.length; v++){
+				result += path[i].vec[v] +' ';
+				// if(v%2 == 0) result += ',';
+			}
+		}
+		// Eliminar l'últim espai
+		return result;
+	}
+
+	scale(value){
+		var path = this.path;
+		for(var i=0; i<path.length; i++){
+			for(var v=0; v<path[i].vec.length; v++){
+				path[i].vec[v] *= value;
+			}
+		}
+	}
+
+	simetryH(){
+		var path = this.path;
+		for(var i=0; i<path.length; i++){
+			for(var v=0; v<path[i].vec.length; v+=2){
+				path[i].vec[v] *= -1;
+			}
+		}
+	}
+}
+
 function parseText(Sexe){
 	const text = document.getElementById("rawText").value;
-	const path = /\"m.*\"/gm;
-	
-	const regexSplit = /[a-zA-Z]([^a-zA-Z]*)[a-zA-Z]/gm;
-
-	var testText = "M1177 1832 l-947 -947 -64 -375 c-35 -206 -63 -376 -61 -378 1 -1 172 26 380 62 l378 64 948 948 949 949 -312 312 c-172 172 -315 313 -318 313 -3 0 -431 -426 -953 -948z m1038 658 l80 -80 -860 -860 -860 -860 -82 82 -83 83 857 857 c472 472 860 858 863 858 3 0 41 -36 85 -80z m-525 -1195 l-860 -860 -85 85 -86 85 863 857 863 856 83 -81 82 -82 -860 -860z m-995 -914 c-3 -2 -67 -14 -142 -27 l-138 -24 -56 56 -56 55 24 137 c12 75 23 141 23 147 0 5 79 -69 175 -165 96 -96 172 -176 170 -179z";
-
-	var splitPath = regexSplit.exec(testText);
-
-	console.log(splitPath);
-
-
-	var i, ctrl = 0;
-
-	var stringPath
-	for(i=0; i<path.length; i++){
-		stringPath = path[i];
-		while(ctrl < 10000){
-
-			stringPath
-
-
-
-			ctrl++;
-		}
-	}
-
-
-
-	const regexNom = /(?<=(\bNom\s))(\w* ?)+\b/gm,
-		regexCognoms = /(?<=(\bCognoms\s))(\w* ?-?\Ñ?)+\t/gm,
-		regexMobil = /(?<=(\bM\òbil\s))(\+?(\d)+)/gm,
-		regexDataNaixement = /(?<=(\bData\snaixement\s))((\d){2}[/]){2}(\d){4}/gm,
-		regexDNI = /(?<=(\bDNI\s))\-?(\w)*/gm,
-		regexEmail = /(?<=(\bE\-mail\s))[\w.\-?]*@[\w.\-?]*/gm;
-	
-	var Nom = regexNom.exec(text),
-	Cognoms = regexCognoms.exec(text),
-	Mobil = regexMobil.exec(text),
-	DataNaixement = regexDataNaixement.exec(text),
-	DNI = regexDNI.exec(text),
-	Email = regexEmail.exec(text);
-
-	if (Nom) Nom = Nom[0];
-	if (Cognoms) Cognoms = Cognoms[0];
-	if (Mobil) Mobil = Mobil[0];
-	if (DataNaixement) DataNaixement = DataNaixement[0];
-	if (DNI) DNI = DNI[0];
-	if (Email) Email = Email[0];
-
-	Cognoms = Cognoms.substr(0, Cognoms.length-1);
-	Cognoms = Cognoms.split(" ");
-	var Cognom1 = Cognoms[0];
-	var Cognom2 = Cognoms[1];
-	
-	//Si algun camp és nul
-	if (!Nom) alert("El camp Nom té valor nul");
-	if (!Cognoms) alert("El camp Cognoms té valor nul");
-	if (!Mobil){
-		Mobil = "-";
-		alert("El camp Mobil té valor nul");
-	}
-	if (!DataNaixement) alert("El camp DataNaixement té valor nul");
-	// if (!DNI) alert("El camp DNI té valor nul");
-	if (!Email) alert("El camp Email té valor nul");
-
-	//Si el DNI és nul o -
-	if(DNI == "-" || !DNI){
-		DNI = "AAAAAAAAA";
-		alert("No hi ha DNI!");
-	}
-
-	//Si hi ha més de 2 cognoms
-	if(Cognoms.length > 2){
-		var Cognom1 = Cognoms[0];
-		var Cognom2 = Cognoms[Cognoms.length-1];
-		for (var i = 1; i <= Cognoms.length - 2; i++) {
-			Cognom1 = Cognom1+" "+Cognoms[i];
-		}
-		Cognoms = [Cognom1, Cognom2];
-		alert("Hi ha més de 2 cognoms!\nRevisa que estiguin bé!");
-	}
-
-	//Si hi ha només hi ha un cognom
-	if(!Cognom2) Cognom2 = "";
-
-	console.log("Nom: "+Nom+"\n"+
-		"Cognom1: "+Cognom1+"\n"+
-		"Cognom2: "+Cognom2+"\n"+
-		"Mobil: "+Mobil+"\n"+
-		"DataNaixement: "+DataNaixement+"\n"+
-		"DNI: "+DNI+"\n"+
-		"Email: "+Email+"\n"+
-		"Sexe: "+Sexe+" (Home: -1. Dona: 0)\n");
-
-	var result = "document.getElementById(\"ctl00_ContentPlaceHolder1_CampDNI_TxtValor\").value = \""+DNI+"\";\ndocument.getElementById(\"ctl00_ContentPlaceHolder1_CampNombre_TxtValor\").value = \""+Nom+"\";\ndocument.getElementById(\"ctl00_ContentPlaceHolder1_CampApellido1_TxtValor\").value = \""+Cognom1+"\";\ndocument.getElementById(\"ctl00_ContentPlaceHolder1_CampApellido2_TxtValor\").value = \""+Cognom2+"\";\ndocument.getElementById(\"ctl00_ContentPlaceHolder1_CampTelefonoMovil_TxtValor\").value = \""+Mobil+"\";\ndocument.getElementById(\"ctl00_ContentPlaceHolder1_CampEmail1_TxtValor\").value = \""+Email+"\";\ndocument.getElementById(\"ctl00_ContentPlaceHolder1_CampFechaNacimiento_TxtValor\").value = \""+DataNaixement+"\";\ndocument.getElementById(\"ctl00_ContentPlaceHolder1_CampGenero_CboValor\").value = "+Sexe+";\n"
-
+	svg.regexParse(text);
+	// console.log();
+	var result = svg.print();
 	var textArea = document.getElementById("resultCode");
 	textArea.value = result;
 	textArea.style.display = "block";
 	document.getElementById("instruccions2").style.display = "block";
 }
+
+function test(){
+	const text = "M 16.968 1.726 L 0 18.698 l 24.808 24.808 l 16.97 -16.97 L 16.968 1.726 Z";
+	svg.regexParse(text);
+	svg.scale(0.1);
+	svg.print();
+	svg.round(3);
+}
+
+
+
+var svg = new ReaderSVG();
--- misc/SVG Transform/regexSVG.js
+++ misc/SVG Transform/regexSVG.js
@@ -1,66 +1,84 @@
+// text.split(/[a-zA-Z]/);
+
 class VectorSVG{
 	constructor(type, vec){
 		this.type = type;
 		this.vec = vec;
-		this.len = vec.length;
+		// this.len = vec.length;
 	}
 }
 
-function regexParser(){
+class ReaderSVG{
+	constructor(){
+		this.strPath = [];
+		this.path = [];
 
-	const regexSubpath = /m([^z]*)z/gim;
-	const regexSplit = /[a-zA-Z]([^a-zA-Z]*)(?=[a-zA-Z])/gm;
-	const regexFloat = /(-?(\d)+(\.?(\d)+)?)/gm;
-
-	var testText = "M8.484, 0.863L0, 9.349l12.404, 12.404l8.485-8.485L8.484, 0.863z M1.791, 9.349l6.693-6.694 l10.612, 10.613l-6.692, 6.695L1.791, 9.349z";
-	var path = [];
-	var subpath = [];
-
-	var findSubpath = regexSubpath.exec(testText);
-	// console.log(findSubpath);
-	while (findSubpath != null){
-		subpath.push(findSubpath[0]);
-		findSubpath = regexSubpath.exec(testText);
+		var test1 = "M1177 1832 l-947 -947 -64 -375 c-35 -206 -63 -376 -61 -378 1 -1 172 26 380 62 l378 64 948 948 949 949 -312 312 c-172 172 -315 313 -318 313 -3 0 -431 -426 -953 -948z m1038 658 l80 -80 -860 -860 -860 -860 -82 82 -83 83 857 857 c472 472 860 858 863 858 3 0 41 -36 85 -80z m-525 -1195 l-860 -860 -85 85 -86 85 863 857 863 856 83 -81 82 -82 -860 -860z m-995 -914 c-3 -2 -67 -14 -142 -27 l-138 -24 -56 56 -56 55 24 137 c12 75 23 141 23 147 0 5 79 -69 175 -165 96 -96 172 -176 170 -179z";
+		var goma1 = "M8.484, 0.863L0, 9.349l12.404, 12.404l8.485-8.485L8.484, 0.863z M1.791, 9.349l6.693-6.694 l10.612, 10.613l-6.692, 6.695L1.791, 9.349z";
+		var goma2 = "M26.534, 18.555l-4.45-4.451c-0.1-0.101-0.261-0.101-0.357, 0l-8.487,  8.485 c-0.099, 0.099-0.099, 0.259, 0, 0.358l2.711, 2.71c0.039, 0.042, 0.091, 0.066, 0.147, 0.072l3.467, 0.015 c0.076, 0.012, 0.154-0.017, 0.211-0.072l6.759-6.759C26.634, 18.815, 26.634, 18.656, 26.534, 18.555z";
+		this.regexParse(goma1);
+		// this.regexParse(goma2);
 	}
 
-	// console.log(subpath);
-
-	var splitPath;
-	for(var i=0; i<subpath.length; i++){
-		path.push([]);
-		splitPath = regexSplit.exec(subpath[i]);
-		while (splitPath != null){
-			path[i].push(splitPath[0]);
-			splitPath = regexSplit.exec(subpath[i]);
+	regexSearch(text, regex){
+		var result = [];
+		var find = regex.exec(text);
+		while (find != null){
+			result.push(find[0]);
+			find = regex.exec(text);
 		}
+		return result;
 	}
 
-	console.log(path);
-	var strElement, floatNumber, type, vec = [], pathVectorSVG = [];
+	regexParse(strSVG){
+		const regexPathMZ = /m([^z]*)z/gim;
+		const regexSplit = /[a-zA-Z]([^a-zA-Z]*)(?=[a-zA-Z])/gm;
+		const regexFloat = /(-?(\d)+(\.?(\d)+)?)/gm;
 
-	for(var i=0; i<=path.length; i++){
-		subpath = path[i];
-		console.log(subpath);
-		for(var j=0; i<=(subpath).length; i++){
-			pathVectorSVG.push([]);
-			strElement = subpath[j];
-			type = strElement[0];
-			type.toUpperCase();
+		var pathMZ = this.regexSearch(strSVG, regexPathMZ);
+		for(var i=0; i<pathMZ.length; i++){
+			this.strPath.push(this.regexSearch(pathMZ[i], regexSplit));
+		}
 
-			floatNumber = regexFloat.exec(strElement);
-			while(floatNumber != null){
-				vec.push(floatNumber);
-				floatNumber = regexFloat.exec(strElement);
+		var strElement, type, vec;
+
+		for(var i=0; i<this.strPath.length; i++){
+			pathMZ = this.strPath[i];
+			for(var j=0; j<pathMZ.length; j++){
+				strElement = pathMZ[j];
+				type = strElement[0];
+				// type = type.toUpperCase();
+				vec = this.regexSearch(strElement, regexFloat);
+				this.path.push(new VectorSVG(type, vec));
 			}
-
-			pathVectorSVG[i].push(new VectorSVG(type, vec));
+			this.path.push(new VectorSVG('Z', []));
 		}
 	}
 
-	console.log(pathVectorSVG);
+	print(){
+		var result = '';
+		var path = this.path;
+		for(var i=0; i<path.length; i++){
+			result += path[i].type +' ';
+			for(var v=0; v<path[i].vec.length; v++){
+				result += path[i].vec[v] +' ';
+				// if(v%2 == 0) result += ',';
+			}
+		}
+		// Eliminar l'últim espai
+		return result;
+	}
 
-
-
+	scale(value){
+		var path = this.path;
+		for(var i=0; i<path.length; i++){
+			for(var v=0; v<path[i].vec.length; v++){
+				path[i].vec[v] *= value;
+			}
+		}
+	}
 }
 
-regexParser();
+var svg = new ReaderSVG();
+svg.scale(2);
+svg.print();
